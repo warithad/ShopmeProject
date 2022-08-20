ed6f8e8bd2e3325d624c1391ea28195d8409d6cd
warith.ad <warith.ad02@gmail.com>
2022-08-20 21:36:40 +0100

    Implement new admin user form

--- ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.shopme.common.entity.User;
 
@@ -21,5 +22,18 @@ public class UserController {
 		
 		model.addAttribute("listUsers", listUsers);
 		return "users";
+	}
+	
+	@GetMapping("/users/new")
+	public String newUser(Model model) {
+		User user = new User();
+		model.addAttribute("user", user);
+		return "user_form";
+	}
+	
+	@PostMapping("/users/save")
+	public String saveUser(User user) {
+		System.out.println(user);
+		return "redirect:/users";
 	}
 }
