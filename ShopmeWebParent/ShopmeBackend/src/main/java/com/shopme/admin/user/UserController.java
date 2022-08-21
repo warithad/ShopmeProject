cdf3740651cf1ad8163faedf715bc618571235c5
warith.ad <warith.ad02@gmail.com>
2022-08-21 20:43:37 +0100

    Complete create new admin user interface
    Successfully save new admin user with one or multiple roles to database

--- ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserController.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 
 @Controller
@@ -26,14 +28,21 @@ public class UserController {
 	
 	@GetMapping("/users/new")
 	public String newUser(Model model) {
+		List<Role> listRoles = service.listRoles();
+		
 		User user = new User();
+		user.setEnabled(true);
+		
+		model.addAttribute("listRoles", listRoles);
 		model.addAttribute("user", user);
 		return "user_form";
 	}
 	
 	@PostMapping("/users/save")
-	public String saveUser(User user) {
+	public String saveUser(User user, RedirectAttributes redirectAttributes) {
 		System.out.println(user);
+		service.save(user);
+		redirectAttributes.addFlashAttribute("message", "The user has been saved successfully");
 		return "redirect:/users";
 	}
 }
