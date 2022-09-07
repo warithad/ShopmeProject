fac2ac809fbe8b248253859dfcc339192acb2608
warith.ad <warith.ad02@gmail.com>
2022-09-07 21:44:59 +0100

    Implement update user photos functionality

--- ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserController.java
@@ -1,5 +1,6 @@
 package com.shopme.admin.user;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,10 +9,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.shopme.admin.FileUploadUtil;
 import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
+
+import org.springframework.util.StringUtils;
 
 @Controller
 public class UserController {
@@ -41,10 +47,18 @@ public class UserController {
 	}
 	
 	@PostMapping("/users/save")
-	public String saveUser(User user, RedirectAttributes redirectAttributes) {
+	public String saveUser(User user, RedirectAttributes redirectAttributes, 
+			@RequestParam("image") MultipartFile multipartFile) throws IOException {
 		System.out.println(user);
-		service.save(user);
-		redirectAttributes.addFlashAttribute("message", "The user has been saved successfully");
+		System.out.println(multipartFile.getOriginalFilename());
+		
+		String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+		
+		String uploadDir = "user-photos";
+		
+		FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+//		service.save(user);
+//		redirectAttributes.addFlashAttribute("message", "The user has been saved successfully");
 		return "redirect:/users";
 	}
 	
